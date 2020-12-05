7469cc501176918f9b58ef65d132556fd9f29281
PSY <giulianolourencon@gmail.com>
2020-12-05 15:01:26 -0300

    Added styled-components

--- packages/web/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import { AppProps } from 'next/app'
-import React from 'react'
-
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
