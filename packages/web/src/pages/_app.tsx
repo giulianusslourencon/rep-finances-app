de6300676c39120ae1a8392ea3ff2ef3db690a9c
Giuliano <giuliano.lourencon@sflabs.com.br>
2021-01-06 11:02:55 -0300

    Added progress bar while loading screens or waiting fetches to be completed

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -1,12 +1,22 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
+import dynamic from 'next/dynamic'
 import React from 'react'
 
 import theme from '@theme/index'
+import 'nprogress/nprogress.css'
+
+const TopProgressBar = dynamic(
+  () => {
+    return import('@components/TopProgressBar')
+  },
+  { ssr: false }
+)
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={theme}>
+      <TopProgressBar />
       <Component {...pageProps} />
     </ChakraProvider>
   )
