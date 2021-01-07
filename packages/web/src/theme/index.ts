9d3244ac929f6a1abe563a48a376fa08b787cf8f
Giuliano <giuliano.lourencon@sflabs.com.br>
2021-01-07 16:00:07 -0300

    Updated theme folder adding foundations

--- packages/web/src/theme/index.ts
+++ packages/web/src/theme/index.ts
@@ -1,12 +1,12 @@
 import { extendTheme } from '@chakra-ui/react'
 
-import { colors } from './colors'
 import { Button } from './components/button'
 import { Cash } from './components/cash'
 import { Heading } from './components/heading'
 import { IdBox } from './components/idBox'
 import { Text } from './components/text'
-import { fonts } from './fonts'
+import { colors } from './foundations/colors'
+import { fonts } from './foundations/fonts'
 import { styles } from './styles'
 
 const customTheme = extendTheme({
