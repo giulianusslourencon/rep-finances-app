2f305ce7139b29e6f5a7f065254d9f50aa731be0
Giuliano <giuliano.lourencon@sflabs.com.br>
2021-01-09 08:42:01 -0300

    Fixed error on mapping payers in the creation screen

--- packages/web/src/pages/transactions/new.tsx
+++ packages/web/src/pages/transactions/new.tsx
@@ -153,7 +153,13 @@ const CreateTransaction: React.FC = () => {
     changeUserOnItem(selectedItemIndex, newRelated)
     setNewRelated('')
     setRelated(currentRelated.sort())
-    setPayers(currentPayers)
+    setPayers(
+      currentPayers.sort((a, b) => {
+        if (a.userId < b.userId) return -1
+        if (a.userId > b.userId) return 1
+        return 0
+      })
+    )
   }
 
   const addNewItem = () => {
