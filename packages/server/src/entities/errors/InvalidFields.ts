ea18904813362fbd2847746d762edb5edac45d83
Giuliano <giuliano.lourencon@sflabs.com.br>
2021-04-06 20:28:47 -0300

    Splitted field keys class from invalid fields file

--- packages/server/src/entities/errors/InvalidFields.ts
+++ packages/server/src/entities/errors/InvalidFields.ts
@@ -4,17 +4,3 @@ export type InvalidFields = {
   field?: string
   error: InvalidError
 }[]
-
-export class FieldKeys {
-  static addKeyOnErrorFields(
-    key: string,
-    errors: InvalidFields
-  ): InvalidFields {
-    return errors.map(error => {
-      return {
-        error: error.error,
-        field: `${key}${error.field ? `.${error.field}` : ''}`
-      }
-    })
-  }
-}
--- packages/server/src/entities/errors/index.ts
+++ packages/server/src/entities/errors/index.ts
@@ -1,3 +1,4 @@
+export * from './FieldKeys'
 export * from './IErrorReason'
 export * from './InvalidError'
 export * from './InvalidFields'
